24399c4ffad43f494053e7fdfcd5baf0ca787330
Daniel <airfan@abv.bg>
2019-10-15 16:46:14 +0300

    Added connection string

--- Data/StoreDbContext.cs
+++ Data/StoreDbContext.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text;
 
 namespace Data
 {
     public class StoreDbContext:DbContext
     {
+        protected IConfigurationRoot configuration;
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Employee> Employees { get; set; }
@@ -17,12 +22,16 @@ namespace Data
         {
 
         }
-       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeeCustomers>().HasOne(x => x.Employee).WithMany(x => x.EmployeeCustomers).OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<EmployeeCustomers>().HasOne(x => x.Customer).WithMany(x => x.EmployeeCustomers).OnDelete(DeleteBehavior.Restrict);
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=airfan\SQLEXPRESS;Database=WebApiCore;Trusted_Connection=True;");
+            configuration = new ConfigurationBuilder().SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)).AddJsonFile("appsettings.json").Build();
+            optionsBuilder.UseSqlServer(this.configuration.GetConnectionString("DefaultConnection"));          
         }
 
     }
 }
- 
